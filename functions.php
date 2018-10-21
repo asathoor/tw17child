0bf9084eee731bd75db58a8c3fce0ea9b7463905
Per Thykjaer Jensen (PETJ) <asathoor@gmail.com>
2018-10-21 19:45:38 +0200

    mapbox-page

--- functions.php
+++ functions.php
@@ -3,7 +3,10 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
 function theme_enqueue_styles() {
-    wp_enqueue_style( 'Mother Stylesheet', get_template_directory_uri() . '/style.css' ); 
+    wp_enqueue_style( 'Mother Stylesheet', get_template_directory_uri() . '/style.css' );
+
+    // Mapbox css
+    wp_enqueue_style( 'Mapbox Stylesheet', 'https://api.tiles.mapbox.com/mapbox-gl-js/v0.50.0/mapbox-gl.css' );
 }
 
 /* improve performance */
--- page-jquery-test.php
+++ page-jquery-test.php
@@ -1,44 +1,57 @@
-<?php 
+<?php
 /**
  * Template: Jquery Sample (name the file after the slug)
  * Purpose: trying out the REST API
  * @package: WordPress
- * @sub-package: Twenty Seventeen 
+ * @sub-package: Twenty Seventeen
  * @sub-package: tw17child (Child theme)
  */
 
 get_header(); ?>
 
 <div class="wrap">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
 
-		
-			<h2> <?php _e('Here\'s a costum page'); // translation ready string ?> </h2>
+      <h2> <?php _e('Map'); // translation ready string ?> </h2>
 
-			<!-- the loop goes here -->
+      <!-- the  goes here -->
+      <div id='map'></div>
+      <div id="text"></div>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+    </main><!-- #main -->
+  </div><!-- #primary -->
 </div><!-- .wrap -->
 
+<script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.50.0/mapbox-gl.js'></script>
+<!-- link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.50.0/mapbox-gl.css' rel='stylesheet' / -->
+
 <script>
+  /**
+   * ADD CONTENT VIA JQUERY
+   * @url: https://developer.wordpress.org/themes/advanced-topics/javascript-best-practices/
+   * Note: that's the way to add Jquery functions in a theme.
+   */
+  (function($) { // document ready
 
-/**
- * Here we add the content via Jquery, not PHP.
- * @url: https://developer.wordpress.org/themes/advanced-topics/javascript-best-practices/
- * Note: that's the way to add Jquery functions in a theme.
- */
-( function( $ ) {
+      /////
 
-	// your code from here ...
-	
-	console.log('Jquery: all systems up and running. Alert level: green.');
+      $('#map').css({
+        width: '100%',
+        height: '225px'
+      });
 
-	// code end
+      mapboxgl.accessToken = 'pk.eyJ1IjoiYXNhdGhvb3IiLCJhIjoiY2oyd3hlbzU3MDA5NzJxbm9iMjczanJndCJ9.HahDB7Z1rrD5THIYQh6t4g';
+      const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/asathoor/cj9iskjc63b9t2sl7p9z7nxxc',
+      center: [10.21,56.15],
+      zoom: 12.0
+      });
 
-} )( jQuery ); // jquery end
+      /////
 
+  })(jQuery); // document ready end
 </script>
 
 <?php get_footer(); ?>
